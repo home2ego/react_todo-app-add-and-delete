34cc2184f53add8770d8d406034345cc011a70b1
Artem Zhyrnyi <ic3ego@gmail.com>
2025-01-19 21:15:45 +0200

    initial load todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import { useEffect, useState } from 'react';
 
-const USER_ID = 0;
+import { getTodos, USER_ID } from './api/todos';
+import { Todo } from './types/Todo';
+import { FilterOptions } from './types/FilterOptions';
+import { ErrorOptions } from './types/ErrorOptions';
+
+import UserWarning from './UserWarning';
+import Header from './components/Header';
+import TodoList from './components/TodoList';
+import Footer from './components/Footer';
+import TodoError from './components/TodoError';
+
+type OptionsParams = {
+  filterOption: FilterOptions;
+};
+
+function getFilteredTodos(todos: Todo[], options: OptionsParams) {
+  let copyTodos = [...todos];
+
+  switch (options.filterOption) {
+    case FilterOptions.ACTIVE:
+      copyTodos = copyTodos.filter(todo => !todo.completed);
+      break;
+    case FilterOptions.COMPLETED:
+      copyTodos = copyTodos.filter(todo => todo.completed);
+      break;
+    case FilterOptions.ALL:
+      break;
+  }
+
+  return copyTodos;
+}
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filterOption, setFilterOption] = useState(FilterOptions.ALL);
+  const [errorOption, setErrorOption] = useState(ErrorOptions.NONE);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => setErrorOption(ErrorOptions.LOAD));
+  }, []);
+
+  const filteredTodos = getFilteredTodos(todos, { filterOption });
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header todos={todos} />
+        <TodoList filteredTodos={filteredTodos} />
+
+        {/* Hide the footer if there are no todos */}
+        {todos.length > 0 && (
+          <Footer
+            todos={todos}
+            filterOption={filterOption}
+            onFilter={setFilterOption}
+          />
+        )}
+      </div>
+
+      <TodoError errorOption={errorOption} onError={setErrorOption} />
+    </div>
   );
 };
